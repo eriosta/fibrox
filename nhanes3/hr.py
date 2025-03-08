b2ddb78c7844d1defffc92745615d1cc08e78f7d
eriosta <eri@sienaspace.com>
2025-03-08 05:34:42 -0600

    mult covars hr

--- nhanes3/hr.py
+++ nhanes3/hr.py
@@ -8,26 +8,6 @@ import xgboost as xgb
 # Load and preprocess the data
 df = pd.read_csv("nhanes3_masld_mortality.csv").dropna(subset=['mortstat', 'NFS', 'FIB4'])
 
-# Age -- Age at interview (screener) - qty
-# Gender -- Sex
-# Race -- Race-ethnicity
-# Smoking -- Have you smoked 100+ cigarettes in life
-# Income -- Poverty Income Ratio (unimputed income)
-# Education -- Highest grade or yr of school completed
-# Hypertension -- is_hypertension
-# Diabetes -- is_diabetes
-# CRP? -- Serum C-reactive protein (mg/dL)
-
-# def print_columns_containing_keyword(df, keyword):
-#     """Print column names that contain the specified keyword sorted by the number of missing values."""
-#     columns_with_keyword = [col for col in df.columns if keyword.lower() in col.lower()]
-#     columns_sorted = sorted(columns_with_keyword, key=lambda col: df[col].isna().sum())
-#     print(columns_sorted)
-
-# # Call the function
-# print_columns_containing_keyword(df, 'race')
-
-
 df_subset = df[[
     'SEQN', 'Alanine aminotransferase:  SI (U/L)', 'Platelet count', 
     'Aspartate aminotransferase: SI(U/L)', 'Glycated hemoglobin: (%)', 'Body mass index', 'mortstat', 
@@ -40,6 +20,12 @@ df_subset = df[[
     'Serum C-reactive protein (mg/dL)'
 ]]
 
+# Drop rows where NaN values are present in the specified columns
+df_subset = df_subset.dropna(subset=[
+    'Age at interview (screener) - qty', 'Sex', 
+    'Race-ethnicity', 'Have you smoked 100+ cigarettes in life',
+    'Poverty Income Ratio (unimputed income)', 'Highest grade or yr of school completed', 'is_hypertension', 'is_diabetes'
+])
 
 
 # Filter out records where ucod_leading is 4
@@ -60,8 +46,7 @@ index_cols = ['SEQN', 'mortstat', 'ucod_leading', 'permth_exm', 'NFS', 'FIB4']
 df_subset = df_subset.set_index(index_cols).dropna()
 
 # Load XGBoost model and make predictions
-# with open(r"C:\Users\erios\masldai_mortality\xgboost_model_isF3_12_21_2024.pkl", 'rb') as file:
-with open("fibrox.pkl", 'rb') as file:
+with open(r"C:\Users\erios\masldai_mortality\xgboost_model_isF3_Youden_Index.pkl", 'rb') as file:
     model = pickle.load(file)
 
 features = [
@@ -81,10 +66,7 @@ y_pred_proba = model.predict(data_dmatrix)
 
 # Define cutoffs and create predictions
 thresholds = {
-    # '90_spec': 0.45588,
-    # '95_spec': 0.52211,
-    # '99_spec': 0.64806,
-    '50_50': 0.5
+    'Youden': 0.507
 }
 
 # Add predictions to dataframe
@@ -101,21 +83,37 @@ df_subset_reset['isnfsmod'] = (df_subset_reset['NFS'] >= -1.455).astype(int)
 df_subset_reset['isnfshigh'] = (df_subset_reset['NFS'] >= 0.676).astype(int)
 df_subset_reset['is_cardiac_mortality'] = df_subset_reset['ucod_leading'].isin([1, 5]).astype(int)
 df_subset_reset['is_malignancy_mortality'] = df_subset_reset['ucod_leading'].isin([2]).astype(int)
+df_subset_reset['is_renal_mortality'] = df_subset_reset['ucod_leading'].isin([9]).astype(int)
+df_subset_reset['is_diabetes_mortality'] = df_subset_reset['ucod_leading'].isin([7]).astype(int)
+df_subset_reset['is_pneumonia_mortality'] = df_subset_reset['ucod_leading'].isin([8]).astype(int)
 
 for spec, threshold in thresholds.items():
     df_subset_reset[f'FibroX_{spec}'] = (y_pred_proba >= threshold).astype(int)
 
 # Create final mortality analysis dataset
 df_mort = df_subset_reset[[
-    'mortstat', 'is_cardiac_mortality', 
-    'FibroX_50_50',
-    'Age (years)', 'Sex', 'permth_exm',
+    'mortstat', 'is_cardiac_mortality', 'is_malignancy_mortality', 'is_renal_mortality', 'is_diabetes_mortality', 'is_pneumonia_mortality',
+    'Prediction_Youden',
     'isnfsmod', 'isnfshigh',
-    'isfib4mod', 'isfib4high'
-]]
+    'isfib4mod', 'isfib4high',
+    'Age (years)', 'Sex', 'permth_exm',
+    'Race-ethnicity', 'Have you smoked 100+ cigarettes in life',
+    'Poverty Income Ratio (unimputed income)', 'Highest grade or yr of school completed', 'is_hypertension', 'is_diabetes', 
+    'Serum C-reactive protein (mg/dL)'
+]].rename(columns={
+    'Age (years)': 'age',
+    'Sex': 'gender',
+    'Race-ethnicity': 'race',
+    'Have you smoked 100+ cigarettes in life': 'smoked_100_cigarettes',
+    'Poverty Income Ratio (unimputed income)': 'poverty_income_ratio',
+    'Highest grade or yr of school completed': 'education_level',
+    'Serum C-reactive protein (mg/dL)': 'crp'
+})
 
+df_mort['permth_exm'].describe()/12
 
-def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, threshold_value=None):
+def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, 
+                          covariates=None, threshold_value=None):
     """
     Run survival analysis for specified follow-up period and model type
     
@@ -124,26 +122,43 @@ def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, thres
         follow_up_years: Follow-up period in years (10, 20, or 30)
         model_type: One of 'FibroX_90_spec', 'FibroX_95_spec', 'FibroX_99_spec', 
                    'isfib4mod', 'isfib4high'
-        event_col: Column name for mortality event ('mortstat' or 'is_cardiac_mortality')
+        event_col: Column name for mortality event ('mortstat', 'is_cardiac_mortality', 
+                   'is_malignancy_mortality', 'is_renal_mortality', 'is_diabetes_mortality', 
+                   'is_pneumonia_mortality')
+        covariates: List of covariate column names to include in the model
         threshold_value: Threshold value to show in legend (e.g. 1.3 for FIB-4 moderate)
     """
-    # Filter by follow-up period
-    df = df_mort[df_mort['permth_exm'] <= 12*follow_up_years]
-    df['time'] = df['permth_exm'] / 12
-    df = df.rename(columns={'Age (years)': 'Age', 'Sex': 'Sex'})
     
+    # Filter by follow-up period
+    df = df_mort[df_mort['permth_exm'] <= 12 * follow_up_years].copy()
+    df['time'] = df['permth_exm'] / 12
+
     # Select relevant columns
-    df = df[['time', event_col, model_type, 'Age', 'Sex']].dropna()
+    df = df[['time', event_col, model_type] + covariates].dropna()
     
     # Fit Cox model
     cph = CoxPHFitter()
     
     # Use splines for all models
+    covariate_formula = " + ".join([f"C({cov})" if df[cov].dtype == 'object' else cov for cov in covariates])
     cph.fit(df, duration_col='time', event_col=event_col,
-            formula=f"Age + C(Sex) + {model_type}")
-    
+            formula=f"{covariate_formula} + {model_type}"
+    )
     # Check proportional hazards assumption and save to file
-    event_type = f"{follow_up_years}-Year All-Cause Mortality" if event_col == "mortstat" else f"{follow_up_years}-Year Cardiovascular Mortality"
+    if event_col == "mortstat":
+        event_type = f"{follow_up_years}-Year All-Cause Mortality"
+    elif event_col == "is_cardiac_mortality":
+        event_type = f"{follow_up_years}-Year Cardiovascular Mortality"
+    elif event_col == "is_malignancy_mortality":
+        event_type = f"{follow_up_years}-Year Malignancy Mortality"
+    elif event_col == "is_renal_mortality":
+        event_type = f"{follow_up_years}-Year Renal Mortality"
+    elif event_col == "is_diabetes_mortality":
+        event_type = f"{follow_up_years}-Year Diabetes Mortality"
+    elif event_col == "is_pneumonia_mortality":
+        event_type = f"{follow_up_years}-Year Pneumonia Mortality"
+    else:
+        event_type = f"{follow_up_years}-Year Mortality"
     
     with open('assumption_checks.txt', 'a') as f:
         f.write(f"\n\nResults for {model_type} - {follow_up_years} year follow-up - {event_type}:\n")
@@ -168,10 +183,9 @@ def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, thres
         plt.gca().xaxis.set_major_locator(plt.MultipleLocator(3))
     
     # Set legend based on model type
-    if model_type.startswith('FibroX'):
-        spec = model_type.split('_')[1].split('spec')[0]
-        plt.legend([f"FibroX P<0.{spec}", 
-                   f"FibroX P≥0.{spec}"])
+    if model_type.startswith('Prediction_Youden'):
+        plt.legend(["FibroX Score <0.507", 
+                   "FibroX Score ≥0.507"])
     elif model_type.startswith('isfib4'):
         plt.legend([f"FIB-4 <{threshold_value}", f"FIB-4 ≥{threshold_value}"])
     elif model_type.startswith('isnfs'):
@@ -182,7 +196,7 @@ def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, thres
     plt.xlim(0, follow_up_years)
     
     # Save plot
-    mortality_type = 'all_cause' if event_col == 'mortstat' else 'cardiovascular'
+    mortality_type = 'all_cause' if event_col == 'mortstat' else 'cardiovascular' if event_col == 'is_cardiac_mortality' else 'malignancy' if event_col == 'is_malignancy_mortality' else 'renal' if event_col == 'is_renal_mortality' else 'diabetes' if event_col == 'is_diabetes_mortality' else 'pneumonia'
 
     os.makedirs('survival_plots', exist_ok=True)
     plt.savefig(f'survival_plots/{model_type}_{mortality_type}_{follow_up_years}yr.png')
@@ -206,12 +220,15 @@ def run_survival_analysis(df_mort, follow_up_years, model_type, event_col, thres
 open('assumption_checks.txt', 'w').close()
 
 # Run analyses for different follow-up periods and models
-follow_up_periods = [10,20,30]
-fibrox_models = ['FibroX_50_50']
+follow_up_periods = [20,30]
+fibrox_models = ['Prediction_Youden']
 nfs_models = [('isnfsmod', -1.455), ('isnfshigh', 0.676)]
 fib4_models = [('isfib4mod', 1.3), ('isfib4high', 2.67)]
 event_cols = ['mortstat', 'is_cardiac_mortality']
 
+covariates = ['age', 'gender', 'race', 
+                'smoked_100_cigarettes', 'poverty_income_ratio', 'education_level', 
+                'is_hypertension', 'is_diabetes', 'crp']
 # Store results
 results = []
 
@@ -219,31 +236,33 @@ results = []
 for event_col in event_cols:
     for years in follow_up_periods:
         for model in fibrox_models:
-            results.append(run_survival_analysis(df_mort, years, model, event_col))
+            results.append(run_survival_analysis(df_mort, years, model, event_col, covariates))
 
 # NFS models        
 for event_col in event_cols:
     for years in follow_up_periods:
         for model, threshold in nfs_models:
-            results.append(run_survival_analysis(df_mort, years, model, event_col, threshold))
+            results.append(run_survival_analysis(df_mort, years, model, event_col, covariates, threshold))
 
 # FIB-4 models
 for event_col in event_cols:
     for years in follow_up_periods:
         for model, threshold in fib4_models:
-            results.append(run_survival_analysis(df_mort, years, model, event_col, threshold))
+            results.append(run_survival_analysis(df_mort, years, model, event_col, covariates, threshold))
 
 # Create summary dataframe
 summary_df = pd.DataFrame(results)
 print("\nSummary of all analyses:")
+# summary_df = summary_df[summary_df['p<0.05'] == True]
+
 print(summary_df)
 
 # Rename models for output
 summary_df['Model'] = summary_df['Model'].replace({
-    'FibroX_50_50': 'FibroX',
+    'Prediction_Youden': 'FibroX',
     'isnfsmod': 'NFS >-1.455',
     'isnfshigh': 'NFS >=0.676',
     'isfib4mod': 'FIB-4 >1.3',
     'isfib4high': 'FIB-4 >=2.67'
 })
-summary_df.to_csv('age_sex_adjusted_mortality.csv', index=False)
+summary_df.to_csv('adjusted_mortality.csv', index=False)
